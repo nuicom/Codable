ee430318da7672d7701399c29e47d7398e69939d
Metas Umsuwan <metas.umsuwan@gmail.com>
2017-12-30 20:39:12 +0700

    Initial Commit

---
+++ CodableProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CodableProject
+//
+//  Created by Metas Umsuwan on 30/12/2560 BE.
+//  Copyright © 2560 Metas Umsuwan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
